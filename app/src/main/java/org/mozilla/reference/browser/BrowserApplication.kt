cea20e01f1707f54b0a6a4ba4e1eaa7bdb770729
Christian Sadilek <christian.sadilek@gmail.com>
2019-04-17 14:30:04 -0400

    Repro ac-2751

--- app/src/main/java/org/mozilla/reference/browser/BrowserApplication.kt
+++ app/src/main/java/org/mozilla/reference/browser/BrowserApplication.kt
@@ -6,6 +6,7 @@ package org.mozilla.reference.browser
 
 import android.app.Application
 import android.content.Context
+import mozilla.components.concept.fetch.Client
 import mozilla.components.service.glean.Glean
 import mozilla.components.service.glean.config.Configuration
 import mozilla.components.support.base.facts.register
@@ -28,7 +29,7 @@ open class BrowserApplication : Application() {
 
         setupCrashReporting(this)
 
-        val megazordEnabled = setupMegazord()
+        val megazordEnabled = setupMegazord(components)
         setupLogging(megazordEnabled)
 
         if (!isMainProcess()) {
@@ -94,7 +95,7 @@ private fun setupCrashReporting(application: BrowserApplication) {
  *
  * @return Boolean indicating if we're in a megazord.
  */
-private fun setupMegazord(): Boolean {
+private fun setupMegazord(components: Components): Boolean {
     // mozilla.appservices.ReferenceBrowserMegazord will be missing if we're doing an application-services
     // dependency substitution locally. That class is supplied dynamically by the org.mozilla.appservices
     // gradle plugin, and that won't happen if we're not megazording. We won't megazord if we're
@@ -104,8 +105,9 @@ private fun setupMegazord(): Boolean {
     // See https://github.com/mozilla-mobile/reference-browser/pull/356.
     return try {
         val megazordClass = Class.forName("mozilla.appservices.ReferenceBrowserMegazord")
-        val megazordInitMethod = megazordClass.getDeclaredMethod("init")
-        megazordInitMethod.invoke(megazordClass)
+        val megazordInitMethod = megazordClass.getDeclaredMethod("init", Lazy::class.java)
+        val client: Lazy<Client> = lazy { components.core.client }
+        megazordInitMethod.invoke(megazordClass, client)
         true
     } catch (e: ClassNotFoundException) {
         Logger.info("mozilla.appservices.ReferenceBrowserMegazord not found; skipping megazord init.")
